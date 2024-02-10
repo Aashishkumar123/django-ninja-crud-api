70d0d052f5cd370afd8afc5b128612d576ee64bf
Aashish <aashishkumar12376@gmail.com>
2024-02-10 22:18:27 +0530

    [added] Health api.

--- ninjacrud/api.py
+++ ninjacrud/api.py
@@ -2,3 +2,8 @@ from ninja import NinjaAPI, Swagger
 
 api = NinjaAPI(docs=Swagger())
 api.add_router("/students/", "main.api.router")
+
+
+@api.get("/")
+def check_health(request):
+    return {"status_code": "200", "message": "ok"}
